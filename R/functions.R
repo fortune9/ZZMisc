aeb978ec2cf4eca2b2c668718600734e68658d81
Zhenguo Zhang <zhangz.sci@gmail.com>
2021-05-06 06:05:42 +0000

    initial commit

---
+++ R/functions.R
@@ -0,0 +1,109 @@
+# string processing
+## random string
+rand_str<-function(length=10) {
+  part1<-paste(sample(c(letters), 2), collapse = "")
+  part2<-paste(sample(c(letters, 0:9), length-2, replace=T), collapse = "")
+  paste0(part1,part2)
+}
+
+## a temp file name
+get_tmp_file<-function(tmpDir=".", prefix="",suffix="", sep="") {
+    tmpFile<-paste(prefix, rand_str(), suffix, sep=sep)
+    file.path(tmpDir, tmpFile)
+}
+
+get_temp_file=get_tmp_file
+
+## wrap text strings
+wrap_text<-function(..., lineLen=72, indent=0, exdent=0) {
+    s<-paste(..., collapse=" ")
+    s<-gsub("\\n+", " ", s)
+    s<-gsub("\\s+", " ", s)
+    s<-sub("^\\s+","",s)
+    s<-sub("\\s+$","",s)
+    s<-strwrap(s, width=lineLen, indent=indent, exdent=exdent)
+    return(paste(s, collapse="\n"))
+}
+
+# I/O
+## Report information
+#
+info<-function(...)
+{
+        message(sprintf("[%s] %s", Sys.time(), paste(..., collapse=" ") ))
+}
+
+warn<-function(...)
+{
+    info("[WARN]", ...)
+}
+
+#' Load a package
+#'
+#' Load a package, and install it if not installed yet
+#'
+#' @param pkg A string for package name
+#'
+#' @importFrom utils install.packages
+#'
+#' @export
+load_package<-function(pkg)
+{
+	if(!requireNamespace(pkg, quietly=T))
+	{
+		message(sprintf("Package %s doesn't exist; installing it now", pkg))
+		install_package(pkg)
+	}
+	#message(sprintf("Package %s is successfully loaded", pkg))
+    require(pkg, character.only=T)
+}
+
+cran_install<-function(pkg) {
+		suppressMessages(
+			install.packages(
+				pkg,
+				dependencies=T,
+				quiet=T,
+				repos="https://cloud.r-project.org"
+				)
+			)
+		# return True if sucess, False if failed
+	    requireNamespace(pkg, quietly=T)
+}
+
+bioc_install<-function(pkg) {
+	if (!requireNamespace("BiocManager", quietly = TRUE)) {
+    	suppressMessages(
+			install.packages("BiocManager",
+				quiet=T,
+				repos="https://cloud.r-project.org"
+				)
+			)
+	}
+	suppressMessages(BiocManager::install(pkg, update=F))
+	# return True if sucess, False if failed
+	requireNamespace(pkg, quietly=T)
+}
+
+#' Install packages from CRAN or Bioconductor
+#'
+#' A convenient function to install R packages from CRAN or
+#' Bioconductor. It tries both repositories and throws failure if
+#' failed in both cases.
+#' 
+#' @param pkg A length-one character vector containing package name
+#' @export
+install_package<-function(pkg)
+{
+	if(cran_install(pkg)) {
+		return(T)
+	} else if(bioc_install(pkg)) {
+		return(T)
+	} else {
+		stop(sprintf("Package %s can't be installed from CRAN and BioConductor",
+			pkg)
+		)
+	}
+}
+
+
---
+++ README.md
@@ -0,0 +1,62 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# ZZMisc
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+This package hosts any functions or utilities that have not found their
+specific packages.
+
+## Installation
+
+You can install this package with the following command from github
+
+``` r
+library(devtools)
+install_github("ZZMisc", "fortune9")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(ZZMisc)
+# load a package, and install it if not installed yet
+load_package("ggplot2")
+#> Loading required package: ggplot2
+## basic example code
+```
+
+## Package website
+
+The associated package website built with
+[pkgdown](https://pkgdown.r-lib.org/) is available at
+[here](https://fortune9.github.io/ZZMisc/)
+
+<!-- comments
+
+What is special about using `README.Rmd` instead of just `README.md`? You can include R chunks like so:
+
+
+```r
+summary(cars)
+#>      speed           dist       
+#>  Min.   : 4.0   Min.   :  2.00  
+#>  1st Qu.:12.0   1st Qu.: 26.00  
+#>  Median :15.0   Median : 36.00  
+#>  Mean   :15.4   Mean   : 42.98  
+#>  3rd Qu.:19.0   3rd Qu.: 56.00  
+#>  Max.   :25.0   Max.   :120.00
+```
+
+You'll still need to render `README.Rmd` regularly, to keep `README.md` up-to-date. `devtools::build_readme()` is handy for this. You could also use GitHub Actions to re-render `README.Rmd` every time you push. An example workflow can be found here: <https://github.com/r-lib/actions/tree/master/examples>.
+
+In that case, don't forget to commit and push the resulting figure files, so they display on GitHub and CRAN.
+-->
+
+## Contributors
+
+Zhenguo Zhang *Author*
